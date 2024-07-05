ee63ff2b37bd9dbf445a2ef915fc49b47dff2d3f
Anushka Gupta <anushkagupta319@gmail.com>
2024-07-05 23:39:14 +0530

    user craeted

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ import authRoute from "./routes/auth.route.js"
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,32 @@
-export const register = (req,res) =>{
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+export const register = async(req,res) =>{
+
+    const {username,email,password}=req.body;
+//console.log(req.body);
+
+
+    //hash the password
+    //create a new user
+    //save the user in the database
+    //return the user
+
     
+
+    const hashedPassword = await bcrypt.hash(password,10);
+    console.log(hashedPassword);
+
+    //create the user in the database
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password:hashedPassword
+        }
+    
+    })
+    res.json(newUser);
+    console.log(newUser);
      
 }
 export const login = (req,res) =>{
