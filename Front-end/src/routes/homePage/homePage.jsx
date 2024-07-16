3cc414eea3f1916d8cad1078ded3c059367fd1ee
Anushka Gupta <anushkagupta319@gmail.com>
2024-07-16 23:21:37 +0530

    commit

--- Front-end/src/routes/homePage/homePage.jsx
+++ Front-end/src/routes/homePage/homePage.jsx
@@ -6,12 +6,13 @@ function HomePage() {
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <h1 className="title">Where Dreams Come Home🏡</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
+          <li>Welcome to urbanNest, where we bring you the best of urban living. </li>
+          <li>Explore our curated listings and find the perfect home that matches your lifestyle and dreams.</li> 
+          <li>Whether you are looking for a chic apartment, a cozy loft, or a spacious family home, urbanNest is your trusted partner in finding the ideal nest in the city.</li>
+
+
           </p>
           <SearchBar />
           <div className="boxes">
