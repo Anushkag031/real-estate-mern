be0dd32fb30cc1faa3c06d0a947ecfd837326bcf
Anushka Gupta <anushkagupta319@gmail.com>
2024-08-01 01:44:59 +0530

    users fetching

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,11 @@
+import prisma from "../lib/prisma.js";
+
 export const getUsers = async (req, res) => {
     console.log("getUsers");
 
   try {
-   // const users = await User.find();
-   // res.status(200).json(users);
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Failed to get Users" });
   }
