e34337f4773ce9625f66f367c2c0d0b745bc4d3a
Anushka Gupta <anushkagupta319@gmail.com>
2024-08-01 02:00:35 +0530

    update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,8 +11,22 @@ export const getUsers = async (req, res) => {
   }
 };
 export const updatedUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId=req.userId;
+  const body=req.body;
+
+  if(id!==tokenUserId)
+  {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
   try {
-    //const users = await prisma.user.findUnique();
+    const updatedUser= await prisma.user.update({
+      where: {
+        id
+      },
+      data:body
+    });
+    res.status(200).json(updatedUser);
    // res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: "Failed to update Users" });
